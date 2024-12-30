e9063ddf9ca9c1bc79a377e910c4fe875f8f8678
BugHunter-X <askzenai@gmail.com>
2024-12-30 14:34:05 +0100

    Add MyClass.scala file

---
+++ MyClass.scala
@@ -0,0 +1,14 @@
+```scala
+class MyClass(val name: String) {
+  private var _age: Int = 0
+
+  def age: Int = _age
+  def age_=(newAge: Int): Unit = {
+    if (newAge >= 0) {
+      _age = newAge
+    } else {
+      throw new IllegalArgumentException("Age cannot be negative")
+    }
+  }
+}
+```
